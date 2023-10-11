879f9b376f529b74ceaaeb17714fd3a85140e33e
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-10-11 09:24:45 -0400

    s

---
+++ object.js
@@ -0,0 +1,30 @@
+const things = [
+    {
+        firstName: "Jimothy",
+        lastName: "Mcpicker",
+        gender: "male",
+        numbers: [1,2,3,5],
+    },
+    {
+        firstName: "Pipper",
+        lastName: "Potty",
+        gender: "female",
+        numbers: [1,2,3,5],
+    },
+    {
+        firstName: "Smooth",
+        lastName: "Crimminal",
+        gender: "male",
+        numbers: [1,2,3,5],
+    },
+    {
+        firstName: "Hit",
+        lastName: "By",
+        gender: "male",
+        numbers: [1,2,3,5],
+    },
+]
+
+things.forEach((gender) => {
+    console.log(gender);
+});
